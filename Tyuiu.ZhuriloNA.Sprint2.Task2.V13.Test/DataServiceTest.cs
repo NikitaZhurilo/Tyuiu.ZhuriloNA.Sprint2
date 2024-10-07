25b4c96e1a18cb5d8ebf26e02f9759ed1c4e2b7b
Tyuiu.ZhuriloNA@outlook.com <121nikit121@gmail.com>
2024-10-08 00:02:24 +0500

    Task2

---
+++ Tyuiu.ZhuriloNA.Sprint2.Task2.V13.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+using Tyuiu.ZhuriloNA.Sprint2.Task2.V13.Lib;
+namespace Tyuiu.ZhuriloNA.Sprint2.Task2.V13.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int x = 5;
+            int y = 6;
+            bool res = ds.CheckDotInShadedArea(x, y);
+            bool waight = true;
+            Assert.AreEqual(true, res);
+        }
+    }
+}
